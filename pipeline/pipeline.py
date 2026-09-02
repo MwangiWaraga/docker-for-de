fd1045f97ae1c36111c570ccb54667d48b97d51a
jmwangi <jmwangi@watuafrica.com>
2026-09-02 13:53:00 +0300

    Added first pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,8 +1,14 @@
-import sys 
-import pandas
+import sys
+import pandas as pd
 
-print('arguments:', sys.argv)
+print("arguments", sys.argv)
 
-month = int(sys.argv[1])
+day = int(sys.argv[1])
+print(f"Running pipeline for day {day}")
 
-print(month)
+
+
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+print(df.head())
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
